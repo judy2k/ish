b05b0c295e1bbd4cfebe32742185a72600612984
Mark Smith <mark.smith@practicalpoetry.co.uk>
2015-09-22 17:20:30 +0100

    Work on packaging and quality

--- setup.py
+++ setup.py
@@ -1,7 +1,58 @@
+#!/usr/bin/env python
+
+import sys
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+REQUIREMENTS = []
+
+TEST_REQUIREMENTS = [
+    "pytest>=2.7.2",
+]
+
+class PyTest(TestCommand):
+    user_options = []
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 setup(
     name='ish',
-    modules=['ish'],
-    author="Mark Smith <mark.smith@practicalpoetry.co.uk>",
+    version="0.0.0",
+    description="Make Python more like more sensible languages.",
+    url="https://github.com/judy2k/fictionary",
+
+    author="Mark Smith",
+    author_email="<mark.smith@practicalpoetry.co.uk>",
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ],
+
+    py_modules=['ish'],
+    install_requires=REQUIREMENTS,
+    zip_safe=False,
+
+    cmdclass={'test': PyTest},
+    tests_require=TEST_REQUIREMENTS,
 )
