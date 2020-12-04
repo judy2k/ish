b9c37b463342bbd8acf0f834499e64b69426009b
Mark Smith <judy@judy.co.uk>
2020-12-04 11:17:08 +0000

    Drop Python 2 support.

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ TEST_REQUIREMENTS = [
     "pytest>=2.7.2",
 ]
 
+
 class PyTest(Command):
     user_options = []
 
@@ -20,35 +21,30 @@ class PyTest(Command):
 
     def run(self):
         import pytest
+
         sys.exit(pytest.main([]))
 
+
 setup(
-    name='ish',
-    version="0.0.0",
+    name="ish",
+    version="0.0.1",
     description="Make Python more like more sensible languages.",
     url="https://github.com/judy2k/ish",
-
     author="Mark Smith",
-    author_email="<mark.smith@practicalpoetry.co.uk>",
-
+    author_email="<judy@judy.co.uk>",
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: Public Domain",
         "Natural Language :: English",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-
-    py_modules=['ish'],
+    py_modules=["ish"],
     install_requires=REQUIREMENTS,
     zip_safe=False,
-
-    cmdclass={'test': PyTest},
+    cmdclass={"test": PyTest},
     tests_require=TEST_REQUIREMENTS,
 )
